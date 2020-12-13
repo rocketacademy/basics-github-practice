01815833ea17c5b75f86997275e311dbe7870963
Jerome CHUA <jeromechua@google.com>
2020-12-13 16:24:34 +0800

    Complete Secret Word App V2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// Secret Word V1
+// Secret Word V2
 
-var counter = 0;
-var guesses = 4;
+// Initialise variables for score, number of guesses, truth value of current/previous user input,
+// and finally an array to keep track of all guesses
+var scoreCounter = 0;
+var guessCounter = 4; // Max: 4x guesses
+var currentGuessBool;
+var previousGuessBool;
+var guessArray = [];
 
 // 3 Secret words
 const secretWords = ['banana', 'chisel', 'faucet'];
@@ -16,45 +21,66 @@ var randomWordSelector = function () {
 
 // Run main function
 var main = function (input) {
-  var myOutputValue = 'This round, your guess was ' + input;
+  var myOutputValue = 'This round, your guess was:<br>' + input;
 
   // App to generate random word
   var secretWord = randomWordSelector();
-  // secretWord = 'faucet'; // Early return
+  secretWord = 'faucet'; // Set an early return for debugging
 
-  // If the user input matches the secret word, a point is added to the counter
+  // If the user input matches the secret word, a point is added to the scoreCounter
   if (input == secretWord) {
-    counter += 1;
+    scoreCounter += 1;
+    guessCounter -= 1;
+    currentGuessBool = true;
+  // If the user input does not the secret word, a guess point is deducted
+  } else if (input != secretWord) {
+    guessCounter -= 1;
+    currentGuessBool = false;
   }
 
-  // If the user input does not the secret word, a guess point is deductecd
-  if (input != secretWord) {
-    guesses -= 1;
-  }
+  // Append every guess to an array initialised above
+  guessArray.push(currentGuessBool);
+  previousGuessBool = guessArray[guessArray.length - 2];
 
-  // To win, user must guess correctly twice
-  if (counter == 2) {
-    myOutputValue = "You've won! You guessed the secret word twice correctly!";
-  }
-
-  // To lose, user 4 guesses becomes zero
-  if (guesses == 0) {
+  // Conditions are placed purposefully in this 1-3 order to output properly
+  // 1) To lose, user's 4 guesses run out aka turns ti zero
+  if (guessCounter == 0) {
     myOutputValue = "You've lost! You ran out of guesses!<br> Please try again.";
   }
+  // 2) If guess right twice but not in a row, & guesses run out, they lose
+  if (scoreCounter == 2 && guessCounter == 0) {
+    myOutputValue = "You've lost! You did not manage to guess correctly twice in a row & you've run out of guesses!";
+  }
+  // 3) To win, user must guess correctly twice IN A ROW
+  if (currentGuessBool == true && previousGuessBool == true) {
+    myOutputValue = "You've won! You guessed the secret word twice correctly in a row!<br> Game will reset in 3 seconds..";
+    setTimeout(() => {
+      window.location.reload(true);
+    }, 3000);
+  }
 
-  var gameInfo = `<br>This round's secret word was: ${secretWord}<br>
-                  Number of correct matches: ${counter}/2 <br>
-                  Number of guesses left: ${guesses}/4 <br>`;
+  var gameInfo = `<br><br> This round's secret word was:<br> ${secretWord}<br>
+                  <br> Number of correct matches:<br> ${scoreCounter} / 2 <br>
+                  <br> Number of guesses left:<br> ${guessCounter} / 4 <br>`;
 
   // Debugging
   console.log('Input');
   console.log(input);
   console.log('Secret Word');
   console.log(secretWord);
-  console.log('Counter');
-  console.log(counter);
-  console.log('Guesses');
-  console.log(guesses);
+  console.log('Score Counter');
+  console.log(scoreCounter);
+  console.log('Guess Counter');
+  console.log(guessCounter);
+  console.log('Current Guess Correct?');
+  console.log(currentGuessBool);
+  console.log('Guess Array');
+  console.log(guessArray);
+  console.log('Guess Array Length');
+  console.log(guessArray.length);
+  console.log('Previous Guess');
+  console.log(previousGuessBool);
 
+  // Return output plus game information
   return myOutputValue + gameInfo;
 };
