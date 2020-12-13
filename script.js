f43e37316cc3f63e386e4b5c4168afda7073e670
Jerome CHUA <jeromechua@google.com>
2020-12-13 16:25:25 +0800

    Complete Secret Word App V3

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Secret Word V2
+// Secret Word V3
 
-// Initialise variables for score, number of guesses, truth value of current/previous user input,
-// and finally an array to keep track of all guesses
+/* Initialise variables for score, guesses, boolean of current, previous, one-before-last input,
+& an arrayto keep track of all guesses */
 var scoreCounter = 0;
 var guessCounter = 4; // Max: 4x guesses
 var currentGuessBool;
 var previousGuessBool;
+var oneBeforePreviousGuessBool;
 var guessArray = [];
 
 // 3 Secret words
@@ -19,13 +20,22 @@ var randomWordSelector = function () {
   return randomWord;
 };
 
+// Generate random number from 2 to 4
+var randomNumber = function () {
+  const randInt = Math.floor(Math.random() * 2) + 2;
+  return randInt;
+};
+
+// Generate a random number for the number of times needed to guess correctly in a row
+const n = randomNumber();
+
 // Run main function
 var main = function (input) {
   var myOutputValue = 'This round, your guess was:<br>' + input;
 
-  // App to generate random word
+  // Generate random word
   var secretWord = randomWordSelector();
-  secretWord = 'faucet'; // Set an early return for debugging
+  // secretWord = 'faucet'; // Set an early return for debugging
 
   // If the user input matches the secret word, a point is added to the scoreCounter
   if (input == secretWord) {
@@ -40,27 +50,61 @@ var main = function (input) {
 
   // Append every guess to an array initialised above
   guessArray.push(currentGuessBool);
+  // Extract the previous guess' value's boolean value
   previousGuessBool = guessArray[guessArray.length - 2];
+  // Extract the one before previous guess' value's boolean value
+  oneBeforePreviousGuessBool = guessArray[guessArray.length - 3];
 
-  // Conditions are placed purposefully in this 1-3 order to output properly
+  // Conditions are placed purposefully in this order to output properly:
   // 1) To lose, user's 4 guesses run out aka turns ti zero
   if (guessCounter == 0) {
-    myOutputValue = "You've lost! You ran out of guesses!<br> Please try again.";
+    myOutputValue = "You've lost! You ran out of guesses!<br> Please try again. <br> Game resets in 3 seconds..";
+    setTimeout(() => {
+      window.location.reload(true);
+    }, 4000);
   }
   // 2) If guess right twice but not in a row, & guesses run out, they lose
   if (scoreCounter == 2 && guessCounter == 0) {
-    myOutputValue = "You've lost! You did not manage to guess correctly twice in a row & you've run out of guesses!";
-  }
-  // 3) To win, user must guess correctly twice IN A ROW
-  if (currentGuessBool == true && previousGuessBool == true) {
-    myOutputValue = "You've won! You guessed the secret word twice correctly in a row!<br> Game will reset in 3 seconds..";
+    myOutputValue = "You've lost! You did not manage to guess correctly twice in a row & you've run out of guesses!<br> Game resets in 3 seconds..";
     setTimeout(() => {
       window.location.reload(true);
-    }, 3000);
+    }, 4000);
+  }
+  // 3) To win, user must guess correctly n times in a row
+  // a) n = 2
+  if (n == 2) {
+    if (currentGuessBool == true && previousGuessBool == true) {
+      myOutputValue = "You've won! You guessed the secret word twice correctly in a row! Good! <br> Game will reset in 3 seconds..";
+      setTimeout(() => {
+        window.location.reload(true);
+      }, 4000);
+    }
+  }
+
+  // b) n = 3
+  if (n == 3) {
+    // eslint-disable-next-line max-len
+    if (currentGuessBool == true && previousGuessBool == true && oneBeforePreviousGuessBool == true) {
+      myOutputValue = "You've won! You guessed the secret word 3 times correctly in a row! Great! <br> Game will reset in 3 seconds..";
+      setTimeout(() => {
+        window.location.reload(true);
+      }, 4000);
+    }
+  }
+
+  // c) n = 4
+  if (n == 4) {
+    // eslint-disable-next-line max-len
+    if (guessArray[0] == true && guessArray[1] == true && guessArray[2] == true && guessArray[3] == true) {
+      myOutputValue = "You've won! You guessed the secret word 4 times correctly in a row! Amazing! <br> Game will reset in 3 seconds..";
+      setTimeout(() => {
+        window.location.reload(true);
+      }, 3000);
+    }
   }
 
   var gameInfo = `<br><br> This round's secret word was:<br> ${secretWord}<br>
-                  <br> Number of correct matches:<br> ${scoreCounter} / 2 <br>
+                  <br> Number of correct matches:<br> ${scoreCounter} / ${n} <br>
                   <br> Number of guesses left:<br> ${guessCounter} / 4 <br>`;
 
   // Debugging
@@ -80,6 +124,10 @@ var main = function (input) {
   console.log(guessArray.length);
   console.log('Previous Guess');
   console.log(previousGuessBool);
+  console.log('Previous Previous Guess');
+  console.log(oneBeforePreviousGuessBool);
+  console.log('Number of guesses needed to be correct in a row');
+  console.log(n);
 
   // Return output plus game information
   return myOutputValue + gameInfo;
