7b17999500e38d092b122d76cc8650b018472568
eddiejpot <jamesporteredward@gmail.com>
2020-12-14 00:00:51 +0800

    added winning logic and cleaned up code (Secret Word X in a Row)

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ var secretWordTwo = 'chisel';
 var secretWordThree = 'faucet';
 var chosenSecretWord = '';
 // make game counter
-var gameCounter = -1;
-// Score board
-var scoreBoard = 0;
+var gameCounter = 0;
+// game details
+var gameDetails = '';
 
+/* --------------CREATE WIN LOGIC (Player has to have 2-4 correct guesses in a row)---------- */
+var scoreBoard = 0;
+var randomScoreboard = function () {
+  // generate a random number from 2 - 4
+  var randomScoreboardNum = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
+  return randomScoreboardNum;
+};
+// generate winning logic
+var winningLogic = randomScoreboard();
+
+/* ----------------------RANDOM WORD GENERATOR LOGIC---------------------------------------- */
 // Random number generator from 1 - 3 *Numbers correspond to chosen secret word
 // Ensure that randomNumber generated does not repeat
 var previousNum = '';
@@ -32,6 +43,8 @@ var randomNumGenerator = function () {
     }
   }
   previousNum = randomNum;
+  // TEST ALWAYS RETURN 1
+  randomNum = 1;
   return randomNum;
 };
 
@@ -51,21 +64,22 @@ var secretWord = function () {
   return chosenSecretWord; // this outputs either banana, chisel or faucet
 };
 
+/* ----------------------USER GUESS LOGIC---------------------------------------- */
 // Check if user guessed the secret word
 var guessCorrect = function (input) {
   var correctAnswer = '';
-  // wrong answer
+  // user gets wrong answer
   if ((input != chosenSecretWord)) {
     // ensure that score doesn't go below zero
     if (scoreBoard <= 0) {
       scoreBoard = 0;
       correctAnswer = `Nope Try again! The secret word was ${chosenSecretWord}`;
     }
-    // ensure that user has to guess correct answer twice in a row
+    // ensure that user has to guess correct answer consecutively
     if (scoreBoard > 0) {
       correctAnswer = `Nope Try again! The secret word was ${chosenSecretWord}`;
-      // minus 1 point
-      scoreBoard -= 1;
+      // restart scoreboard
+      scoreBoard = 0;
     }
   }
   // correct answer 1
@@ -86,41 +100,52 @@ var guessCorrect = function (input) {
     // add to scoreboard
     scoreBoard += 1;
   }
+  // Check if user has won the game
+  if (scoreBoard == winningLogic) {
+    correctAnswer = 'NICE! Time to start a new game!';
+  }
   return correctAnswer;
 };
 
 // How many more correct guesses to win
-var guessesToWin = 2;
+var guessesToWin = winningLogic;
 var guessesToWinChange = 0;
 var howManyGuessesToWin = function () {
   var guessesToWinStatement = '';
   if (scoreBoard == 0) {
-    guessesToWinStatement = 'You need 2 more correct guesses to win!';
+    guessesToWinStatement = `${scoreBoard}/${winningLogic}`;
   }
-  if ((scoreBoard > 0) && (guessesToWin <= 2)) {
+  if ((scoreBoard > 0) && (guessesToWin <= winningLogic)) {
     guessesToWinChange = (guessesToWin - scoreBoard);
-    guessesToWinStatement = `You need ${guessesToWinChange} more correct guesses to win!`;
+    guessesToWinStatement = `${scoreBoard}/${winningLogic}`;
   }
   if (scoreBoard >= guessesToWin) {
-    guessesToWinStatement = 'You already won twice!';
+    // Start a new game
+    scoreBoard = 0;
+    gameDetails = 'You won! Start a new game' + '<br>' + '<br>';
+    winningLogic = randomScoreboard();
   }
   return guessesToWinStatement;
 };
 
-// Main function
+/* --------------------------------------------------------------------------- */
+/* ----------------------MAIN FUNCTION---------------------------------------- */
+/* --------------------------------------------------------------------------- */
 var main = function (input) {
   var myOutputValue = '';
   // gameCounter
   gameCounter += 1;
   // generate secret word
   secretWord();
+  // Display instructions for game
+  gameDetails = 'Winning Condition:' + '<br>' + `${winningLogic} correct guesses in a row` + '<br>' + '<br>';
   // START GAME. Check if user guessed the secret word
-  myOutputValue = guessCorrect(input) + '<br>' + howManyGuessesToWin();
+  myOutputValue = 'Result:' + '<br>' + guessCorrect(input) + '<br>' + '<br>' + 'Scoreboard:' + '<br>' + howManyGuessesToWin();
   // TEST scoreboard
   console.log('THE Score Board ' + scoreBoard);
   // TEST gameCounter
   console.log(`Games played: ${gameCounter}`);
   // TEST previousNum
   console.log(`Previous Number: ${previousNum}`);
-  return myOutputValue;
+  return gameDetails + myOutputValue;
 };
