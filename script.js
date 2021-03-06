4040cad4073985ad99a5eccd8c7044149fa3ad87
ishaan.malhi <ish.malhi@gmail.com>
2021-03-06 14:46:53 +0800

    Completed the task

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// var main = function (input) {
+//   var myOutputValue = 'hello world';
+//   return myOutputValue;
+// };
+
+var correctGuessCount = 0;
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  const Food = ['Banana', 'Chisel', 'faucet'];
+  const random = Math.floor(Math.random() * Food.length);
+  var requiredGuessesToWin = 2;
+  var myOutputValue = 'you lose :(';
+
+  console.log(random, Food[random]);
+  if (input == Food[random]) {
+    correctGuessCount = correctGuessCount + 1;
+    console.log(correctGuessCount, 'number of correct guesses');
+
+    myOutputValue = `Nice, you guessed ${Food[random]}. You have ${correctGuessCount} correct guesses.`;
+  }
+  if (correctGuessCount == requiredGuessesToWin) {
+    myOutputValue = 'You have won, awesome!';
+  }
   return myOutputValue;
 };
