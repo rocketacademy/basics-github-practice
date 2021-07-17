322f6498006d221afa34990eb88e0bcedb16c06e
whyexis <chongyingxiang@gmail.com>
2021-07-17 14:33:43 +0800

    Added new function to randomly generate number of correct guesses in a row needed to win

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var BANANA = "banana";
 var CHISEL = "chisel";
 var FAUCET = "faucet";
 var numCorrectGuesses = 0;
-var winCondition = 2;
+var winCondition = generateWinCondition();
 
 var randomWord = function () {
   var randomDecimal = Math.random() * 3;
@@ -19,8 +19,15 @@ var randomWord = function () {
   }
 };
 
+function generateWinCondition() {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  return randomInteger + 2;
+}
+
 var main = function (input) {
   var secretWord = randomWord();
+
   if (input == secretWord) {
     numCorrectGuesses += 1;
   }
@@ -30,6 +37,8 @@ var main = function (input) {
   }
 
   if (numCorrectGuesses == winCondition) {
+    numCorrectGuesses = 0;
+    winCondition = generateWinCondition();
     return "Congrats, you won after guessing the secret word correctly twice in a row.";
   }
 
