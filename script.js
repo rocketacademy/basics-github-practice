6e929a7a12aba076129d7c19ab27d3f8d75e699f
tanzheen <ntan418@gmail.com>
2021-07-20 11:15:13 +0800

    secret word twice in a row 2

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
-var getDiceNumber = function () {
-  return 1;
-
-  var diceNumber = Math.random() * 6;
-  var diceInteger = Math.ceil(diceNumber);
-  console.log(`dice roll = ${diceInteger}`);
-  return diceInteger;
-};
-
-var getWithinNumber = function () {
-  var withinNumber = Math.random() * 3;
-  var withinInteger = Math.ceil(withinNumber);
-
-  return withinInteger;
-};
-// i will make a game for each within number
-var playDiceWithinNumber = function (input, withinDiceNumber, dice1, dice2) {
-  var upperlimit1 = dice1 + withinDiceNumber;
-  var lowerlimit1 = dice1 - withinDiceNumber;
-  var lowerlimit2 = dice2 - withinDiceNumber;
-  var upperlimit2 = dice2 + withinDiceNumber;
-  if ((input <= upperlimit1 && input >= lowerlimit1) || (input <= upperlimit2 && input >= lowerlimit2)) {
-    wincount = wincount + 1;
-
-    var outputSentence = `YOU WON! DICE 1 = ${dice1}. DICE 2 = ${dice2}. WITHIN NUMBER = ${withinDiceNumber}. INPUT = ${input}`;
-    wincount = 0;
-    return outputSentence;
-  }
-  wincount = wincount + 1;
-  return `YOU LOSE! DICE 1 = ${dice1}. DICE 2 = ${dice2}. WITHIN NUMBER = ${withinDiceNumber}. INPUT = ${input}`;
-};
-// At the beginning of the game and each time the player wins, the computer selects a random number from 1 to 3, which we call the "within number"
-var wincount = 0;
-
-var withinDiceNumber = 0;
 var main = function (input) {
-  console.log(`wincount = ${wincount}`);
   console.log(`input = ${input}`);
-  if (wincount == 0) {
-    withinDiceNumber = getWithinNumber();
-    console.log(`within ${withinDiceNumber}`);
-    var dice1 = getDiceNumber();
-    var dice2 = getDiceNumber();
-    var playgame = playDiceWithinNumber(input, withinDiceNumber, dice1, dice2);
-    return playgame;
-  } else if (wincount != 0) {
-    var dice1 = getDiceNumber();
-    var dice2 = getDiceNumber();
-    var playgame = playDiceWithinNumber(input, withinDiceNumber, dice1, dice2);
-    return playgame;
+  computerWord = getComputerWord();
+  console.log(`computer word = ${computerWord}`);
+  var startGame1 = playGame(input, computerWord);
+  return startGame1;
+};
+var winStreak = 0;
+var computerWord = 0;
+var lastComputerWord = 0;
+//this gets the computer output
+var getComputerWord = function () {
+  var number = Math.random() * 3;
+  var integer = Math.ceil(number);
+
+  if (integer == 1) {
+    return `banana`;
+  }
+  if (integer == 2) {
+    return `chisel`;
+  }
+  if (integer == 3) {
+    return `faucet`;
+  }
+};
+var compareInputAndComputer = function (input, computerWord) {
+  console.log(`last computer word = ${lastComputerWord}`);
+  var output = `computer word = ${computerWord}. last computer word = ${lastComputerWord}`;
+  lastComputerWord = `${computerWord}`;
+
+  if (input == computerWord) {
+    winStreak = winStreak + 1;
+
+    return "you win! " + output;
+  }
+  if (input != computerWord) {
+    return `you lose! ` + output;
+  }
+};
+
+var playGame = function (input, computerWord) {
+  if (winStreak == 0) {
+    var outcome = compareInputAndComputer(input, computerWord);
+    return outcome;
+  }
+  if (winStreak > 0) {
+    if (lastComputerWord == computerWord) {
+      computerWord = getComputerWord();
+      console.log(`computer word = ${computerWord}`);
+      return playGame(input, computerWord);
+    }
+    if (lastComputerWord != computerWord) {
+      var outcome = compareInputAndComputer(input, computerWord);
+      return outcome;
+    }
   }
 };
