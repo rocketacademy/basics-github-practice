81c495b320fdc89643518d248658aa6a30a25b12
Eunice321 <xeuniceyeex@gmail.com>
2021-08-28 15:01:12 +0800

    Secret Word Twice in a Row

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 var prevScore = 0;
+var previousWord = 0;
 
 var main = function (input) {
+  // generate random word
   var randomWord = diceRoll();
-  console.log(`secret = ${randomWord}`);
+  console.log(`secret word = ${randomWord}`);
+  var prevWord = getPreviousWord();
+
+  // update previous word with current word
+  previousWord = randomWord;
+  console.log(`previous word = ${prevWord}`);
 
   var myOutputValue = `You guessed ${input}. The secret word is ${randomWord}. You Lose! Your score is ${prevScore}.`;
 
   if (input == randomWord) {
-    prevScore = prevScore + 1;
-    myOutputValue = `You guessed ${input}. The secret word is ${randomWord}. You Win! Your score is ${prevScore}`;
-    if (prevScore >= 2) {
-      myOutputValue = `You guessed ${input}. The secret word is ${randomWord}. You have officially won the game!!`;
+    // if input is guessed correctly once
+    myOutputValue = `You guessed ${input}. The secret word is ${randomWord}. You need one more guess! Your score is ${prevScore}`;
+    // if input is guessed correctly twice in a row, score is given
+    if (randomWord == prevWord) {
+      prevScore = prevScore + 1;
+      myOutputValue = `You guessed ${input}. The secret word is ${randomWord}. You Win! Your score is ${prevScore}`;
     }
   }
   return myOutputValue;
@@ -29,4 +38,9 @@ var diceRoll = function () {
   if (diceNumber == 2) {
     return "faucet";
   }
+};
+
+// a function to display previous word
+var getPreviousWord = function () {
+  return previousWord;
 };
