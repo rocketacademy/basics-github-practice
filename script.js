ddc43ac18fefa544fff3f5fe1a0f05601b94215b
Bastario1 <kaidekaide@hotmail.com>
2021-08-28 14:14:11 +0800

    Secret Word App

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+var banana = 'banana'
+var chisel = 'chisel'
+var faucet = 'faucet'
+var numOfCorrectGuesses = 0
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var result = checkResult(input)
+  console.log(`player guess ${input}`);
+  return result
+};
+
+
+var getWordRandom = function () {
+  var secretWords = ["banana", "chisel", "faucet"];
+  var randomWord = secretWords[Math.floor(Math.random() * secretWords.length)];
+  return randomWord;
+};
+
+var checkResult = function (input) {
+  var secretWords = getWordRandom ()
+  console.log(`secret word ${secretWords}`);
+
+if (input == secretWords) {
+  numOfCorrectGuesses += 1
+  console.log(`number of correct guesses ${numOfCorrectGuesses}`);
+};
+
+if (numOfCorrectGuesses >= 2) {
+  return `you win!`
+}
+else {return `one more correct guess to win!`};
 };
