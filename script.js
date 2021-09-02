7ca22201002c20ddaebc4023a07d80bf184cd56a
kopp3r <chris.situmorang@gmail.com>
2021-09-02 17:33:49 +0800

    secret word x

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var winCount = 0;
 var guessOptions = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
+  return "banana";
   if (randomInteger == 0) {
     return "banana";
   } else if (randomInteger == 1) {
@@ -14,6 +15,23 @@ var guessOptions = function () {
     return "faucet";
   }
 };
+
+var correctsToWin = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  if (randomInteger == 0) {
+    return 2;
+  } else if (randomInteger == 1) {
+    return 3;
+  } else if (randomInteger == 2) {
+    return 4;
+  }
+};
+
+var staticCorrectsToWin = 1;
+
+var resetScore = function () {};
+
 var isGuessCorrect = function (guess, guessed) {
   if (guess == guessed) {
     winCount += 1;
@@ -25,20 +43,40 @@ var isGuessCorrect = function (guess, guessed) {
 
 var main = function (input) {
   var word = guessOptions();
+  console.log("word");
   console.log(word);
+  console.log("input");
   console.log(input);
   isGuessCorrect(input, word);
-  if (winCount > 2) {
-    winCount -= 1;
+
+  if (staticCorrectsToWin == 1) {
+    staticCorrectsToWin = correctsToWin();
   }
+
+  console.log("winCount");
   console.log(winCount);
+  console.log("staticCorrectsToWin");
+  console.log(staticCorrectsToWin);
+
   var myOutputValue =
     "The secret word is " +
     word +
     ". You guessed " +
     input +
     ".<br> You need " +
-    (2 - winCount) +
+    (staticCorrectsToWin - winCount) +
     " more correct guesses to win.";
+  if (winCount == staticCorrectsToWin) {
+    myOutputValue =
+      "The secret word is " +
+      word +
+      ". You guessed " +
+      input +
+      ".<br> You have guessed " +
+      staticCorrectsToWin +
+      " times correctly. You win!";
+    staticCorrectsToWin = correctsToWin();
+    winCount = 0;
+  }
   return myOutputValue;
 };
