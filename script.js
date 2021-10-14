f958806e81b2bfebf20cc915824390980e81d7ca
Linwt94 <lin_wanting@hotmail.com>
2021-10-14 21:26:19 +0800

    Amend to x round Secret Word Game

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var randomWinsNeeded = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal) + 2;
+
+  return randomInteger;
+};
+
+var winsNeeded = randomWinsNeeded();
+console.log("correct guesses to win=====", winsNeeded);
+
 var randomWordGenerate = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal) + 1;
@@ -21,25 +31,30 @@ var randomWordGenerate = function () {
 var winCount = 0;
 
 var main = function (input) {
-  var secretWord = randomWordGenerate();
-  //var secretWord = "banana";
+  //var secretWord = randomWordGenerate();
+  var secretWord = "banana";
 
-  var myOutputValue = "You lose. You still need 2 correct guesses";
+  var myOutputValue = `You need ${winsNeeded} correct guesses in a row to win`;
 
   console.log("secret word ===", secretWord);
 
-  if (winCount == 1) {
-    myOutputValue =
-      "That was not the secret word. You now have 1 correct guess. Guess it correct again to win.";
+  if (input != secretWord) {
+    winCount = 0;
   }
 
   if (input == secretWord) {
     winCount += 1;
-    myOutputValue = "You have 1 correct guess. Guess it correct again to win.";
+    console.log("curren correct guesses", winCount);
+    myOutputValue = `You have ${winCount} correct guesses. You need need ${
+      winsNeeded - winCount
+    } more correct guesses in a row to win`;
   }
 
-  if (winCount == 2) {
-    myOutputValue = "You have 2 correct guesses. You win!";
+  if (winCount == winsNeeded) {
+    myOutputValue = `You guess correctly ${winsNeeded} time. You win!`;
+    winCount = 0;
+    winsNeeded = randomWinsNeeded();
+    console.log("correct guesses to win in new round=====", winsNeeded);
   }
 
   return (
