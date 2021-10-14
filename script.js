ae8bdb0ecd63efad61410ff1a472112b4d733d67
nat <nati.saver@gmail.com>
2021-10-14 21:14:47 +0800

    added

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ var generate = function () {
 
 var word = generate();
 var gamesWon = 0;
+var guesses = 0;
 
 var main = function (input) {
   if (input == word) {
     gamesWon += 1;
+    guesses +=1;
     word = generate();
+    if (gamesWon == 2) {
+      return "Game Reset " + "Your Guess: " + input + " The word: " + word + " Games Won: " + gamesWon;
+    }
+    else {
+      return "Your Guess: " + input + " The word: " + word + " Games Won: " + gamesWon;
+
+    }
+    
   }
-  return input + " " + word + " " + gamesWon;
+  else{
+    guesses = 0;
+    gamesWon = 0;
+    word = generate();
+    return "That was incorrect. "+ "Your Guess: " + input + " The word: " + word + " Games Won: " + gamesWon;
+  }
+
 };
