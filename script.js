6a45963775baae9ddd8146daa423ddf5e8d4a1b5
richiechia <richiechia@gmail.com>
2021-10-16 14:42:48 +0800

    Added Secret Word X in a Row

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+// Function
+var random_Number = function () {
+  var randomNumber = Math.random() * 3;
+  // range from 0 to 2
+  var integernumber = Math.floor(randomNumber);
+  // range from 2 to 4
+  var number = integernumber + 2;
+  return number;
+};
+
 var counter = 0;
+var round = random_Number();
+console.log(round);
 
 var main = function (input) {
-  var myOutputValue = "hello world";
+  var myOutputValue = "";
 
   var computer_choice = secret_word();
   console.log(computer_choice);
 
   if (input == computer_choice) {
     counter += 1;
-    myOutputValue = "You win! You guessed it correctly " + counter + " time(s)";
+    myOutputValue = "You guessed it correctly " + counter + " time(s)";
   } else {
-    myOutputValue = "You guessed it wrongly!";
+    counter = 0;
+    myOutputValue =
+      "You guessed it wrongly! " + counter + " time(s) correctly.";
+  }
+
+  if (counter == round) {
+    myOutputValue = `You win! You guessed it correctly  ${counter}  time(s)`;
   }
 
   return myOutputValue;
